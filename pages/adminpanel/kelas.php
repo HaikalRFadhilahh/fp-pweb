faacccdb7426979d25b1945e0ae31069e7c20edb
Sulthan Asyraf Donison <96214577+sulthanasyraf12@users.noreply.github.com>
2023-07-11 02:01:06 +0700

    Add files via upload

--- pages/adminpanel/kelas.php
+++ pages/adminpanel/kelas.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../../CSS/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <?php
     include('../../material/icons.php');
     include('../../material/fonts.php');
@@ -20,6 +22,18 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
+        <div class="dropdown">
+            <a style="margin-right:50px"class="btn btn-secondary dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                Profile
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">My profile</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                    </li>
+                </ul>
+        </div>
     </nav>
 
     <div class="row g-0">
@@ -29,6 +43,7 @@
                 <button type="button" class="btn-close bg-white" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
             </div>
            <nav id="sidebarMenu" class="collapse d-md-block sidebar collapse .bg-secondary.bg-gradient">
+   
     <div class="position-sticky">
       <div class="list-group list-group-flush mx-4 mt-7">
 
@@ -64,11 +79,46 @@
   </nav>
         </div>
         <div class="col-lg-10 col-12">
-            <div class="container">
-
+            
+        <div class="container">
+            <div>
+            <div class="row">
+                <div class="col-md-10">
+                <h1>Kelas</h1>
+                </div>
+                <div class="col-md-2 px-5 py-2">
+                <a href="form_tambah.php" class="btn btn-success ">Tambah</a>
+                </div>
             </div>
+            <?php include '../../koneksi/koneksi.php';
+            $sql = "SELECT * FROM kelas";
+            $result = mysqli_query($conn, $sql);
+            ?>
+                    <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nama kelas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                        <tr>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['nama_kelas']; ?></td>
+                            <td>
+                                <a href="form_edit.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a> 
+                                <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Hapus</a> 
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+
+                </div>
         </div>
-    </div>
+                
+           
     <script src="../../JS/bootstrap.min.js"></script>
 </body>
 
