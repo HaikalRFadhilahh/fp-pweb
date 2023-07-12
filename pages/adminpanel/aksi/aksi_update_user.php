3d7f7f3d70e5df3da2ac92226afc4d8c08eb9530
Haikal R Fadhilah <haikal.fadhilah13@gmail.com>
2023-07-12 18:50:55 +0700

    Update User PHP Done

--- pages/adminpanel/aksi/aksi_update_user.php
+++ pages/adminpanel/aksi/aksi_update_user.php
@@ -1,16 +1,31 @@
 <?php
+session_start();
 require('../../../koneksi/koneksi.php');
 $nama = $_POST['nama'];
 $username = $_POST['username'];
 $password = $_POST['password'];
+$id = $_SESSION['id'];
 
-$query = "select * from users where username = '$username'";
+$query = "select * from users where id=$id";
 $res = mysqli_query($conn, $query);
+$data = mysqli_fetch_assoc($res);
 
-if (mysqli_num_rows($res) > 1) {
-    echo "<script>alert('Username Sudah Terpakai,Harap Gunakan Username Lain!!')</script>";
+if ($data >= 1 && $data['username'] != $username) {
+    echo "<script>alert('Username Sudah Di Gunakan!!')</script>";
     echo "<script>document.location='../user.php'</script>";
-} else {
-    echo "<script>alert('Username Masih Tersedia!!')</script>";
+}
+
+try {
+    //code...
+    if ($password != $data['password']) {
+        $password = md5($password);
+    }
+    $queryUpdate = "update users set nama='$nama',username='$username',password='$password' where id=$id";
+    mysqli_query($conn, $queryUpdate);
+    echo "<script>alert('User Profile Berhasil Di Update!!')</script>";
+    echo "<script>document.location='../user.php'</script>";
+} catch (\Throwable $th) {
+    //throw $th;
+    echo "<script>alert('Internal Server Error!!')</script>";
     echo "<script>document.location='../user.php'</script>";
 }
